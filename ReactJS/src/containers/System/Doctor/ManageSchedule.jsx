b0060ed717fb628eab6a322ae48cf98f12594bbc
DinhTuanAnh2k1 <dinhtuananh12072001@gmail.com>
2022-04-06 13:25:20 +0700

    tao bulkcreate voi sequelize

--- ReactJS/src/containers/System/Doctor/ManageSchedule.jsx
+++ ReactJS/src/containers/System/Doctor/ManageSchedule.jsx
@@ -9,6 +9,7 @@ import DatePicker from '../../../components/Input/DatePicker';
 import moment from 'moment';
 import { toast } from "react-toastify";
 import _ from 'lodash';
+import {saveBulkScheduleDoctor} from '../../../services/userService';
 class ManageSchedule extends Component {
 
     constructor(props) {
@@ -93,7 +94,7 @@ class ManageSchedule extends Component {
         }
     }
 
-    handleSaveSchedule =()=>{
+    handleSaveSchedule =async()=>{
         let {rangeTime, selectedDoctor, currentDate} = this.state; 
         let result =[];
         if(selectedDoctor && _.isEmpty(selectedDoctor)){
@@ -104,7 +105,8 @@ class ManageSchedule extends Component {
             toast.error("Invalid Date!")
             return;
         }
-        let formatedDate =moment(currentDate).format(dateFormat.SEND_TO_SERVER);
+        // let formatedDate =moment(currentDate).format(dateFormat.SEND_TO_SERVER);
+        let formatedDate = new Date(currentDate).getTime();
         if( rangeTime && rangeTime.length > 0){
             let selectedTime = rangeTime.filter(item=>
                 item.isSelected===true)
@@ -113,7 +115,7 @@ class ManageSchedule extends Component {
                     let object = {};
                     object.doctorId = selectedDoctor.value;
                     object.date = formatedDate;
-                    object.time = schedule.keyMap;
+                    object.timeType = schedule.keyMap;
                     result.push(object)
                 })
             }
@@ -122,6 +124,11 @@ class ManageSchedule extends Component {
                 return;
             }
         }
+        let res = await saveBulkScheduleDoctor({
+            arrSchedule: result,
+            doctorId: selectedDoctor.value,
+            formatedDate: formatedDate
+        })
 
         console.log('check result', result)
 
