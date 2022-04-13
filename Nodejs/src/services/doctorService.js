9920c68432e6656e25e4bfe57f02d7c6f40bc91e
DinhTuanAnh2k1 <dinhtuananh12072001@gmail.com>
2022-04-14 01:07:12 +0700

    hoan thien chuc nang quan ly lich hen bac si

--- Nodejs/src/services/doctorService.js
+++ Nodejs/src/services/doctorService.js
@@ -1,6 +1,7 @@
 import db from '../models/index';
 require('dotenv').config();
 import _ from 'lodash';
+import emailService from '../services/emailService';
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
@@ -408,6 +409,40 @@ let getListPatientForDoctor = (doctorId, date) => {
     })
 }
 
+let sendRemedy = (data) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if (!data.email || !data.doctorId || !data.patientId || !data.timeType || !data.imageBase64) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters'
+                })
+            } else {
+                let appointment = await db.Booking.findOne({
+                    where: {
+                        doctorId: data.doctorId,
+                        patientId: data.patientId,
+                        timeType: data.timeType,
+                        statusId: 'S2'
+                    },
+                    raw: false
+                })
+                if (appointment) {
+                    appointment.statusId = 'S3';
+                    await appointment.save();
+                }
+                await emailService.sendAttachment(data);
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK'
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -418,4 +453,5 @@ module.exports = {
     getExtraInforDoctorById: getExtraInforDoctorById,
     getProfileDoctorById: getProfileDoctorById,
     getListPatientForDoctor: getListPatientForDoctor,
+    sendRemedy: sendRemedy,
 }
--- Nodejs/src/services/emailService.js
+++ Nodejs/src/services/emailService.js
@@ -52,6 +52,60 @@ let getBodyHTMLEmail = (dataSend) => {
     return result;
 }
 
+let getBodyHTMLEmailRemedy = (dataSend) => {
+    let result = ''
+    if (dataSend.language === 'vi') {
+        result = `
+            <h3>Xin chào ${dataSend.patientName}!</h3>
+            <p>Bạn nhận được email này vì đã đặt lịch khám bệnh online trên TuanAnh88</p>
+            <p>Thông tin đơn thuốc/hóa đơn được gửi trong file đính kèm.</p>
+            
+            <div>Xin chân thành cảm ơn!</div>
+        `
+    }
+    if (dataSend.language === 'en') {
+        result = `
+            <h3>Hi ${dataSend.patientName}!</h3>
+            <p>You received this email because you booked an online medical appointment on TuanAnh88</p>
+            <p>Prescription/invoice information is sent in the attached file.</p>
+            
+            <div>Sincerely thanks!</div>
+        `
+    }
+    return result;
+}
+
+let sendAttachment = async(dataSend) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: process.env.EMAIL_APP, // generated ethereal user
+                    pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+                },
+            });
+            let info = await transporter.sendMail({
+                from: '"TuanAnh88 👻" <tuananh2001ptit@gmail.com>', // sender address
+                to: dataSend.email, // list of receivers
+                subject: "Kết quả khám bệnh ✔", // Subject line
+                html: getBodyHTMLEmailRemedy(dataSend),
+                attachments: [{
+                    filename: `remedy-${dataSend.patientId}-${new Date().getTime()}.png`,
+                    content: dataSend.imageBase64.split("base64,")[1],
+                    encoding: 'base64'
+                }]
+            });
+            resolve(true)
+        } catch (error) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     sendSimpleEmail: sendSimpleEmail,
+    sendAttachment: sendAttachment,
 }
